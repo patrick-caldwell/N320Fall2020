35ad74169e493d9b73f205e79450097fb781ea0b
Patrick <patrickcaldwell1998@gmail.com>
2020-10-23 12:43:57 -0400

    Vue Data Listing Lab

---
+++ labs/lab7/data-listing/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="pageTitle">
+      <h1>Part of my N64 Game Collection</h1>
+      <h4>Because I don't know what else to use</h4>
+    </div>
+    <DataDisplay v-bind:games="this.games" />
+  </div>
+</template>
+
+<script>
+import DataDisplay from './components/DataDisplay.vue'
+
+export default {
+  name: 'App',
+  data: function(){
+    return {
+      games: null,
+    };
+  },
+  components: {
+    DataDisplay
+  },
+  mounted: function(){
+    fetch("data/data.json")
+    .then(response => response.json())
+    .then(jsonData => {
+      console.log(jsonData);
+      this.games = jsonData;
+    });
+  },
+};
+</script>
+
+<style>
+  body {
+    background-color: #404552;
+  }
+
+  #app{
+    color: white;
+    font-family: sans-serif;
+  }
+
+  .pageTitle{
+    margin-left: 50px;
+  }
+</style>
---
+++ labs/lab7/data-listing/src/components/DataDisplay.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="gameContainer">
+        <div class="game" v-for="game in games" :key="game.name">
+            <img class="boxArt" :src="game.image" :alt="game.name" />
+            <div class="gameInfo">
+                <p>{{game.name}}</p>
+                <p>{{game.language}} {{game.region}}</p>
+                <p>Developed by {{game.developer}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+    
+<script>
+export default {
+    props: ["games"],
+    data: function() {
+        return {
+            test: "Hello Hello",
+        };
+    },
+    name: "DataDisplay",
+}
+</script>
+
+<style scoped>
+    .gameContainer{
+        margin-top: 35px;
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+
+    .game{
+        display: flex;
+        margin-bottom: 50px;
+        margin-right: 35px;
+        padding: 25px 17.5px;
+        background-color: #5A5F6D;
+        border-radius: 40px;
+    }
+
+    .boxArt{
+        height: 150px;
+        width: 130px;
+        margin-right: 25px;
+        border-radius: 40px;
+        object-fit: cover;
+    }
+
+    .gameInfo{
+        display: flex;
+        flex-direction: column;
+    }
+</style>
